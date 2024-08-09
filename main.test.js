8c30bdf22a01dfd4b67775b1672790f0cea7cf75
Emilijxa <168300036+Emilijxa@users.noreply.github.com>
2024-08-09 12:48:59 +0200

    Added all test cases for second kata

--- main.test.js
+++ main.test.js
@@ -1,5 +1,11 @@
 import { test, expect } from "vitest";
 import { sumResources } from "./main.js";
+import { convertTemperature } from "./main.js";
+
+
+
+
+// SUM OF ARRAYS TEST CASES
 
 test("should return the sum of resource values", () => {
     const expected = 23;
@@ -25,4 +31,42 @@ test("should work with negative sum", () => {
     expect(actual).toBe(expected);
   });
 
+
+
+
+// TEMPERATURE CONVERTER TEST CASES
+
+test("should correctly convert if it is 0, freezing point of water", () => {
+  const expected = 32;
+  const actual = convertTemperature(0);
+  expect(actual).toBe(expected);
+});
+
+
+test("should correctly convert if it is human body temperature", () => {
+  const expected = 98.6;
+  const actual = convertTemperature(37);
+  expect(actual).toBe(expected);
+});
+
+
+test("should correctly convert if it is room temperature", () => {
+  const expected = 68;
+  const actual = convertTemperature(20);
+  expect(actual).toBe(expected);
+});
+
+
+test("should correctly convert if it is boiling hot water", () => {
+  const expected = 212;
+  const actual = convertTemperature(100);
+  expect(actual).toBe(expected);
+});
+
+
+test("should correctly convert if it is absolutely 0", () => {
+  const expected = -459.4;
+  const actual = convertTemperature(-273);
+  expect(actual).toBe(expected);
+});
 
